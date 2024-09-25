c3b2a849068d6e5a16e0d3689eba56e6e876c189
Daragh Hollman <hollmandaragh@gmail.com>
2024-09-25 13:41:08 +0100

    Fixed philpott spice discrepancies and cleaned up code

--- list_distributions.py
+++ list_distributions.py
@@ -23,10 +23,9 @@ spice.furnsh("/home/daraghhollman/Main/SPICE/messenger/metakernel_messenger.txt"
 
 # Import the crossing interval list
 crossings = boundaries.Load_Crossings(
-    "/home/daraghhollman/Main/mercury/philpott_crossings.p"
+    "/home/daraghhollman/Main/mercury/philpott_2020_reformatted.csv"
 )
 
-crossings = crossings.iloc[50:100]
 
 # Determine distance and local time for each crossing midpoint
 heliocentric_distances = np.empty(len(crossings), dtype=dt.datetime)
@@ -48,32 +47,13 @@ for i in tqdm(range(len(crossings)), total=len(crossings)):
     heliocentric_distances[i] /= 57.91e6
 
     # Local time calculation
-    position_start = np.array(
-        [
-            crossings.iloc[i]["start_x_msm"],
-            crossings.iloc[i]["start_y_msm"],
-            crossings.iloc[i]["start_z_msm"],
-        ]
-    )
-    position_end = np.array(
-        [
-            crossings.iloc[i]["end_x_msm"],
-            crossings.iloc[i]["end_y_msm"],
-            crossings.iloc[i]["end_z_msm"],
-        ]
-    )
-    # Get midpoint position
-    #position_b = (position_start + position_end) / 2
-    position_b = position_start
+    position_start = trajectory.Get_Position("MESSENGER", crossings.iloc[i]["start"])
+    position_end = trajectory.Get_Position("MESSENGER", crossings.iloc[i]["end"])
 
-    position = trajectory.Get_Position("MESSENGER", crossings.iloc[i]["start"])
+    position = position_start
 
     if position is None:
         continue
-
-    pos_a.append(position)
-    pos_b.append(position_b)
-
 
     longitude = np.arctan2(position[1], position[0]) * 180 / np.pi
 
@@ -83,19 +63,6 @@ for i in tqdm(range(len(crossings)), total=len(crossings)):
     local_times[i] = ((longitude + 180) * 24 / 360) % 24
 
 
-pos_a = np.array(pos_a) / 2440
-pos_b = np.array(pos_b)
-fig, ax = plt.subplots()
-ax.scatter(pos_a[:, 0], pos_a[:, 1], color="blue", label="SPICE Locations")
-ax.scatter(pos_b[:, 0], pos_b[:, 1], color="red", label="Philpott Locations")
-ax.plot(pos_a[:, 0], pos_a[:, 1], color="blue", alpha=0.4)
-ax.plot(pos_b[:, 0], pos_b[:, 1], color="red", alpha=0.4)
-plotting.Plot_Mercury(ax, shaded_hemisphere="left")
-ax.set_aspect("equal")
-ax.set_xlabel("X MSM [R$_M$]")
-ax.set_ylabel("Y MSM [R$_M$]")
-ax.legend()
-plt.show()
 # Remove unassigned elements
 heliocentric_distances = heliocentric_distances[heliocentric_distances != None]
 local_times = local_times[local_times != None]

5be675379f1679b63f9b63926c2911e4770457bd
Daragh Hollman <hollmandaragh@gmail.com>
2024-10-02 14:59:17 +0100

    Implimented probability measure for splitting

--- bimodal_tools.py
+++ bimodal_tools.py
@@ -2,9 +2,15 @@
 Tools to aid in the investigation of the bimodal nature of the magnetic field at Mercury's magnetospheric boundaries.
 """
 
-
 import matplotlib.pyplot as plt
-from numpy import ma
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+import numpy as np
+from numpy.typing import ArrayLike
+import pandas as pd
+from scipy.optimize import curve_fit
+import scipy.signal
+
+from plotting_tools import colored_line
 
 
 def Create_Axes():
@@ -47,3 +53,242 @@ def Create_Axes():
     histogram_axis = ax5
 
     return fig, trajectory_axes, mag_axes, histogram_axis
+
+
+class Population_Fit:
+    def __init__(
+        self, x_values: ArrayLike, y_values: ArrayLike, guess_params: list[float]
+    ):
+        self.x_values = x_values
+        self.y_values = y_values
+        self.guess_params = guess_params
+        self.pars, self.cov = self.Get_CurveFit()
+
+        self.population_a_pars = self.pars[:3]
+        self.population_b_pars = self.pars[3:]
+
+        # Useful for plotting
+        self.x_range = np.linspace(x_values[0], x_values[-1], 1000)
+        self.output_y_values = Double_Gaussian(
+            self.x_range,
+            *self.pars,
+        )
+        self.population_a = Single_Gaussian(
+            self.x_range,
+            *self.population_a_pars
+        )
+        self.population_b = Single_Gaussian(
+            self.x_range,
+            *self.population_b_pars
+        )
+
+    def Get_CurveFit(self):
+
+        pars, cov = curve_fit(
+            Double_Gaussian, self.x_values, self.y_values, self.guess_params
+        )
+
+        return pars, cov
+
+
+def Split_Distribution(
+    mag_data: pd.DataFrame,
+    distribution_values: list[float],
+    fit: Population_Fit,
+    fig: plt.Figure,
+    mag_axes: list[plt.Axes],
+    histogram_axis: plt.Axes,
+    method="minimum",
+):
+    """
+    INSERT DOCSTRING HERE
+    """
+
+    match method:
+
+        case "likelihood":
+
+            # For each point in the data, we determine the likelihood that it is
+            # in distribution A and in distribution B.
+            # We take the most likely option as the direction (towards -1 or +1),
+            # and the value between 0 and +/-1.
+            probabilities = []
+            for field_value in distribution_values:
+                
+                # Find the likelihood we're in distribution A and B
+                a_likelihood = Single_Gaussian(field_value, *fit.population_a_pars)
+                b_likelihood = Single_Gaussian(field_value, *fit.population_b_pars)
+
+                probability_point_in_a = a_likelihood / (a_likelihood + b_likelihood)
+                probability_point_in_b = b_likelihood / (a_likelihood + b_likelihood)
+
+                if probability_point_in_a > probability_point_in_b:
+                    probabilities.append(probability_point_in_a)
+
+                elif probability_point_in_a < probability_point_in_b:
+                    probabilities.append(-probability_point_in_b)
+
+                else:
+                    probabilities.append(0)
+
+            line_x = colored_line(
+                mag_data["date"],
+                mag_data["mag_x"],
+                probabilities,
+                ax=mag_axes[0],
+                cmap="bwr",
+            )
+            line_total = colored_line(
+                mag_data["date"],
+                mag_data["mag_total"],
+                probabilities,
+                ax=mag_axes[1],
+                cmap="bwr",
+            )
+
+            # Add an Axes to the right of the main Axes.
+            ax1_divider = make_axes_locatable(mag_axes[0])
+            cax1 = ax1_divider.append_axes("right", size="2%", pad="2%")
+            fig.colorbar(line_x, cax=cax1)
+
+            ax2_divider = make_axes_locatable(mag_axes[1])
+            cax2 = ax2_divider.append_axes("right", size="2%", pad="2%")
+            fig.colorbar(line_total, cax=cax2)
+
+
+        case "midpoint":
+            # Find the midpoint between the two peaks
+            midpoint = (fit.pars[1] + fit.pars[4]) / 2
+
+            region_index = []
+
+            for field_value in distribution_values:
+
+                if field_value > midpoint:
+                    current_region = 1
+                else:
+                    current_region = 0
+
+                region_index.append(current_region)
+
+            colored_line(
+                mag_data["date"],
+                mag_data["mag_x"],
+                region_index,
+                ax=mag_axes[0],
+                cmap="bwr",
+            )
+            colored_line(
+                mag_data["date"],
+                mag_data["mag_total"],
+                region_index,
+                ax=mag_axes[1],
+                cmap="bwr",
+            )
+
+            histogram_axis.axvline(midpoint, color="orange", label="Midpoint")
+
+        case "minimum_point":
+            # Split based off of the minimum point of the gaussian distribution
+            distribution_minimum_index, _ = scipy.signal.find_peaks(-fit.output_y_values)
+
+            distribution_minimum = fit.x_range[distribution_minimum_index]
+
+            region_index = []
+
+            for field_value in distribution_values:
+
+                if field_value > distribution_minimum:
+                    current_region = 1
+                else:
+                    current_region = 0
+
+                region_index.append(current_region)
+
+            colored_line(
+                mag_data["date"], mag_data["mag_x"], region_index, ax=mag_axes[0], cmap="bwr"
+            )
+            colored_line(
+                mag_data["date"],
+                mag_data["mag_total"],
+                region_index,
+                ax=mag_axes[1],
+                cmap="bwr",
+            )
+
+            histogram_axis.axvline(
+                distribution_minimum, color="orange", label="Distribution Minimum"
+            )
+
+        case "threshold":
+            threshold_sigmas = 1
+            # Upper threshold:
+            #       If the data is low, and we go higher than this then we switch.
+            #       Upper threshold is the mean - 1 sigma of the upper gaussian
+            upper_threshold = fit.pars[4] - threshold_sigmas * fit.pars[5]
+
+            # Lower threshold:
+            #       If the data is high, and we go lower than this then we switch.
+            #       Lower threshold is the mean + 1 sigma of the lower gaussian
+            lower_threshold = fit.pars[1] + threshold_sigmas * fit.pars[2]
+
+            region_index = []
+            # Low region = 0, high region = 1
+            # We determine the initial region from a comparison of the first data point
+            if distribution_values[0] > upper_threshold:
+                current_region = 1
+            elif distribution_values[0] < lower_threshold:
+                current_region = 0
+            # We need to handle cases between
+            else:
+                upper_difference = upper_threshold - distribution_values[0]
+                lower_difference = distribution_values[0] - lower_threshold
+
+                if upper_difference >= lower_difference:
+                    current_region = 1
+                else:
+                    current_region = 0
+
+            for field_value in distribution_values:
+
+                if field_value > upper_threshold:
+                    current_region = 1
+                elif field_value < lower_threshold:
+                    current_region = 0
+
+                region_index.append(current_region)
+
+            colored_line(
+                mag_data["date"],
+                mag_data["mag_x"],
+                region_index,
+                ax=mag_axes[0],
+                cmap="bwr",
+            )
+            colored_line(
+                mag_data["date"],
+                mag_data["mag_total"],
+                region_index,
+                ax=mag_axes[1],
+                cmap="bwr",
+            )
+
+            histogram_axis.axvline(
+                upper_threshold, color="red", label="Upper Threshold"
+            )
+            histogram_axis.axvline(
+                lower_threshold, color="blue", label="Lower Threshold"
+            )
+
+
+def Double_Gaussian(x, c1, mu1, sigma1, c2, mu2, sigma2):
+    res = c1 * np.exp(-((x - mu1) ** 2.0) / (2.0 * sigma1**2.0)) + c2 * np.exp(
+        -((x - mu2) ** 2.0) / (2.0 * sigma2**2.0)
+    )
+    return res
+
+
+# We define a single gaussian too to overplot
+def Single_Gaussian(x, c, mu, sigma):
+    res = c * np.exp(-((x - mu) ** 2) / (2 * sigma**2))
+    return res
--- create_mag_histogram.py
+++ create_mag_histogram.py
@@ -14,19 +14,13 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
-import scipy.signal
 import spiceypy as spice
-from scipy.optimize import curve_fit
 
 import bimodal_tools
-from plotting_tools import colored_line
 
 histogram_parameter = "magnitude"  # options: magnitude, x
 fit_curve = True
-split_method = "minimum_point"  # none, threshold, midpoint, minimum_point
-
-# if threshold is selected:
-number_of_sigmas = 3
+split_method = "likelihood"  # none, likelihood, threshold, midpoint, minimum_point
 
 
 # PARAMETERS
@@ -38,8 +32,8 @@ philpott_crossings = boundaries.Load_Crossings(
     "/home/daraghhollman/Main/mercury/philpott_2020_reformatted.csv"
 )
 
-start_time = dt.datetime(year=2012, month=7, day=2, hour=17, minute=20)
-end_time = dt.datetime(year=2012, month=7, day=2, hour=17, minute=40)
+start_time = dt.datetime(year=2011, month=3, day=31, hour=13, minute=35)
+end_time = dt.datetime(year=2011, month=3, day=31, hour=14, minute=15)
 
 # 1: LOAD DATA
 data = mag.Load_Between_Dates(root_dir, start_time, end_time)
@@ -71,9 +65,6 @@ for i, ax in enumerate(mag_axes):
     boundaries.Plot_Crossing_Intervals(ax, start_time, end_time, philpott_crossings)
     ax.set_ylabel(y_labels[i])
 
-    # Plot hline at 0
-    # ax.axhline(0, color="grey", ls="dotted")
-
     ax.set_xmargin(0)
     ax.tick_params("x", which="major", direction="inout", length=16, width=1)
     ax.tick_params("x", which="minor", direction="inout", length=8, width=0.8)
@@ -88,6 +79,8 @@ plotting.Add_Tick_Ephemeris(
 )
 mag_axes[0].set_xticklabels([])
 
+
+# 4: LOADING TRAJECTORY DATA
 # Get trajectory data from spice
 time_padding = dt.timedelta(hours=6)
 spice_dates = [
@@ -118,6 +111,7 @@ padded_positions = trajectory.Get_Trajectory(
 positions /= 2439.7
 padded_positions /= 2439.7
 
+# 5: PLOT TRAJECTORIES
 trajectory_axes[0].plot(
     positions[:, 0], positions[:, 1], color="magenta", lw=3, zorder=10
 )
@@ -151,7 +145,7 @@ trajectory_axes[1].legend(
 )
 
 
-# 4: PLOTTING HISTOGRAM OF B_X
+# 5: PLOTTING BIMODAL HISTOGRAM
 match histogram_parameter:
     case "x":
         histogram_parameter_values = data["mag_x"]
@@ -173,22 +167,21 @@ hist_data, bin_edges, _ = histogram_axis.hist(
     bins=bins,
     density=True,
     color="black",
-    label=f"{start_time.strftime('%Y-%M-%d %H:%M:%S')} to {end_time.strftime('%Y-%M-%d %H:%M:%S')}"
+    label=f"{start_time.strftime('%Y-%m-%d %H:%M:%S')} to {end_time.strftime('%Y-%m-%d %H:%M:%S')}"
     + "\nsampled per second",
 )
 
-bin_centres = bin_edges[:-1] + np.diff(bin_edges) / 2
+histogram_axis.set_ylabel("Probability Density of Measurements")
+histogram_axis.set_xlabel(histogram_axis_label + f" (binsize {binsize} nT)")
+histogram_axis.yaxis.tick_right()
+histogram_axis.yaxis.set_label_position("right")
 
 
-# Get double gaussian fit
-def Double_Gaussian(x, c1, mu1, sigma1, c2, mu2, sigma2):
-    res = c1 * np.exp(-((x - mu1) ** 2.0) / (2.0 * sigma1**2.0)) + c2 * np.exp(
-        -((x - mu2) ** 2.0) / (2.0 * sigma2**2.0)
-    )
-    return res
 
-
+# 6: FITTING A DOUBLE GAUSSIAN TO THE DISTRIBUTION
 if fit_curve:
+    bin_centres = bin_edges[:-1] + np.diff(bin_edges) / 2
+
     curve_fit_guess_params = [
         0.01,
         np.mean(histogram_parameter_values) - np.std(histogram_parameter_values),
@@ -197,159 +190,47 @@ if fit_curve:
         np.mean(histogram_parameter_values) + np.std(histogram_parameter_values),
         5,
     ]
-    pars, cov = curve_fit(
-        # We need to pass some initial guess parameters, these were chosen arbitraraly
-        Double_Gaussian,
-        bin_centres,
-        hist_data,
-        curve_fit_guess_params,
-    )
 
-    fit_range = np.linspace(bins[0], bins[-1], 100)
-    fit_values = Double_Gaussian(
-        fit_range,
-        pars[0],
-        pars[1],
-        pars[2],
-        pars[3],
-        pars[4],
-        pars[5],
-    )
+    population_fit = bimodal_tools.Population_Fit(bin_centres, hist_data, curve_fit_guess_params)
 
     histogram_axis.plot(
-        fit_range,
-        fit_values,
-        color="magenta",
-        lw=3,
+        population_fit.x_range,
+        population_fit.output_y_values,
+        color="grey",
+        lw=10,
+        alpha=0.5,
+        zorder=1,
         label="Double Gaussian Fit",
     )
-
-    match split_method:
-        case "none":
-            pass
-
-        case "threshold":
-            # Upper threshold:
-            #       If the data is low, and we go higher than this then we switch.
-            #       Upper threshold is the mean - 1 sigma of the upper gaussian
-            upper_threshold = pars[4] - number_of_sigmas * pars[5]
-
-            # Lower threshold:
-            #       If the data is high, and we go lower than this then we switch.
-            #       Lower threshold is the mean + 1 sigma of the lower gaussian
-            lower_threshold = pars[1] + number_of_sigmas * pars[2]
-
-            region_index = []
-            # Low region = 0, high region = 1
-            # We determine the initial region from a comparison of the first data point
-            if histogram_parameter_values[0] > upper_threshold:
-                current_region = 1
-            elif histogram_parameter_values[0] < lower_threshold:
-                current_region = 0
-            # We need to handle cases between
-            else:
-                upper_difference = upper_threshold - histogram_parameter_values
-                lower_difference = histogram_parameter_values - lower_threshold
-
-                if upper_difference >= lower_difference:
-                    current_region = 1
-                else:
-                    current_region = 0
-
-            for i, field_value in enumerate(histogram_parameter_values):
-
-                if field_value > upper_threshold:
-                    current_region = 1
-                elif field_value < lower_threshold:
-                    current_region = 0
-
-                region_index.append(current_region)
-
-            colored_line(
-                data["date"], data["mag_x"], region_index, ax=mag_axes[0], cmap="bwr"
-            )
-            colored_line(
-                data["date"],
-                data["mag_total"],
-                region_index,
-                ax=mag_axes[1],
-                cmap="bwr",
-            )
-
-            histogram_axis.axvline(
-                upper_threshold, color="red", label="Upper Threshold"
-            )
-            histogram_axis.axvline(
-                lower_threshold, color="blue", label="Lower Threshold"
-            )
-
-        case "midpoint":
-            # Find the midpoint between the two peaks
-            midpoint = (pars[1] + pars[4]) / 2
-
-            region_index = []
-
-            for i, field_value in enumerate(histogram_parameter_values):
-
-                if field_value > midpoint:
-                    current_region = 1
-                else:
-                    current_region = 0
-
-                region_index.append(current_region)
-
-            colored_line(
-                data["date"], data["mag_x"], region_index, ax=mag_axes[0], cmap="bwr"
-            )
-            colored_line(
-                data["date"],
-                data["mag_total"],
-                region_index,
-                ax=mag_axes[1],
-                cmap="bwr",
-            )
-
-            histogram_axis.axvline(midpoint, color="orange", label="Midpoint")
-
-        case "minimum_point":
-            # Split based off of the minimum point of the gaussian distribution
-            distribution_minimum_index, _ = scipy.signal.find_peaks(-fit_values)
-
-            distribution_minimum = fit_range[distribution_minimum_index]
-
-            region_index = []
-
-            for i, field_value in enumerate(histogram_parameter_values):
-
-                if field_value > distribution_minimum:
-                    current_region = 1
-                else:
-                    current_region = 0
-
-                region_index.append(current_region)
-
-            colored_line(
-                data["date"], data["mag_x"], region_index, ax=mag_axes[0], cmap="bwr"
-            )
-            colored_line(
-                data["date"],
-                data["mag_total"],
-                region_index,
-                ax=mag_axes[1],
-                cmap="bwr",
-            )
-
-            histogram_axis.axvline(
-                distribution_minimum, color="orange", label="Distribution Minimum"
-            )
+    histogram_axis.plot(
+        population_fit.x_range,
+        population_fit.population_a,
+        color="blue",
+        lw=3,
+        zorder=5,
+        label="Population A",
+    )
+    histogram_axis.plot(
+        population_fit.x_range,
+        population_fit.population_b,
+        color="red",
+        lw=3,
+        zorder=5,
+        label="Population B",
+    )
 
 
-histogram_axis.set_ylabel("Probability Density of Measurements")
-histogram_axis.set_xlabel(histogram_axis_label + f" (binsize {binsize} nT)")
-histogram_axis.yaxis.tick_right()
-histogram_axis.yaxis.set_label_position("right")
+    bimodal_tools.Split_Distribution(
+        data,
+        histogram_parameter_values,
+        population_fit,
+        fig,
+        mag_axes,
+        histogram_axis,
+        method=split_method,
+    )
+    histogram_axis.legend()
 
-plt.legend()
 
 plt.savefig(
     f"/home/daraghhollman/Main/mercury/Figures/bimodal/bimodal_{start_time.strftime("%Y_%m_%d__%H_%M_%S")}_{end_time.strftime("%Y_%m_%d__%H_%M_%S")}.png",
